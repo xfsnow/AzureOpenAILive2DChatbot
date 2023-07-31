16df092f2a1cb020735a28087bf4832451026d6d
Feng Xue <xuefeng@snowpeak.org>
2023-07-31 09:54:15 +0800

    修改 max_token 参数，以增加OpenAI返回结果

--- Samples/TypeScript/Demo/src/azureai.ts
+++ Samples/TypeScript/Demo/src/azureai.ts
@@ -37,7 +37,7 @@ export class AzureAi {
     const conversation = conversations + "\n\n## " + prompt
     const m = {
       "prompt": `##${conversation}\n\n`,
-      "max_tokens": 300,
+      "max_tokens": 4000,
       "temperature": 0,
       "frequency_penalty": 0,
       "presence_penalty": 0,
